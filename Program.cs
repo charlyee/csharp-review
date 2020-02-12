20d9b87c2cfee35b2391d11c080a730c237c0c6a
uhrich <warren@sosmediacorp.com>
2020-02-12 11:17:20 -0700

    Initial commit; Added if/ifelse/else chain.

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace C_Review
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 35; // Assigning an int!
+            Console.WriteLine("Hello Warren!"+x);
+
+            if ( x < 30 ) {
+                Console.WriteLine( "X is less than thirty!" );
+            } else if ( x == 30 ) {
+                Console.WriteLine( "X is thirty!" );
+            } else {
+                Console.WriteLine( "X is greater than Thirty!" );
+            }
+        }
+    }
+}
